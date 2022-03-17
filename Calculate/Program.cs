e7cde8053e664b51de0f7760a541a524f1fceb1d
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-03-17 20:13:53 +0300

    Update Program.cs

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using CalculatorLibrary;
 
 namespace CalculatorProgram
 {
-
     class Program
     {
         static void Main(string[] args)
@@ -17,31 +16,9 @@ namespace CalculatorProgram
             while (!endApp)
             {
                 // Объявляем переменные и устанавливаем их пустыми.
-                string numInput1 = "";
-                string numInput2 = "";
+                string number1 = "";
+                string number2 = "";
                 double result = 0;
-
-                // Просим пользователя ввести первое число.
-                Console.Write("Type a number, and then press Enter: ");
-                numInput1 = Console.ReadLine();
-
-                double cleanNum1 = 0;
-                while (!double.TryParse(numInput1, out cleanNum1))
-                {
-                    Console.Write("This is not valid input. Please enter an integer value: ");
-                    numInput1 = Console.ReadLine();
-                }
-
-                // Просим пользователя ввести второе число.
-                Console.Write("Type another number, and then press Enter: ");
-                numInput2 = Console.ReadLine();
-
-                double cleanNum2 = 0;
-                while (!double.TryParse(numInput2, out cleanNum2))
-                {
-                    Console.Write("This is not valid input. Please enter an integer value: ");
-                    numInput2 = Console.ReadLine();
-                }
 
                 // Просим пользователя выбрать оператора.
                 Console.WriteLine("Choose an operator from the following list:");
@@ -49,13 +26,111 @@ namespace CalculatorProgram
                 Console.WriteLine("\ts - Subtract");
                 Console.WriteLine("\tm - Multiply");
                 Console.WriteLine("\td - Divide");
+                Console.WriteLine("\t% - Mod");
+                Console.WriteLine("\tsin - Sinus");
+                Console.WriteLine("\tcos - Cosinus");
+                Console.WriteLine("\ttan - Tangent");
+                Console.WriteLine("\tcot - Cotangent");
+                Console.WriteLine("\tsqrt - Sqrt");
                 Console.Write("Your option? ");
 
-                string op = Console.ReadLine();
+                string operation = Console.ReadLine();
 
+                static void Trigonometry(string operation, string number1 = "", string number2 = "", double result = 0)
+                {
+                    // Просим пользователя ввести первое число.
+                    Calculator calculator = new Calculator();
+                    Console.Write("Type a number, and then press Enter: ");
+                    number1 = Console.ReadLine();
+                    double clearingNumber1 = 0;
+                    double clearingNumber2 = 0;
+                    while (!double.TryParse(number1, out clearingNumber1))
+                    {
+                        Console.Write("This is not valid input. Please enter an integer value: ");
+                        number1 = Console.ReadLine();
+                    }
+                    try
+                    {
+                        result = calculator.DoOperation(clearingNumber1, clearingNumber2, operation);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+                    // Ждем, пока пользователь ответит, прежде чем закрыть.
+                    Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                }
+
+                if (operation == "sqrt")
+                {
+                    Trigonometry(operation, number1, number2, result);
+                    if (Console.ReadLine() == "n") endApp = true;
+                    Console.WriteLine("\n");
+                    calculator.Finish();
+                    return;
+                }
+
+                if (operation == "sin")
+                {
+                    Trigonometry(operation, number1, number2, result);
+                    if (Console.ReadLine() == "n") endApp = true;
+                    Console.WriteLine("\n");
+                    calculator.Finish();
+                    return;
+                }
+
+                if (operation == "cos")
+                {
+                    Trigonometry(operation, number1, number2, result);
+                    if (Console.ReadLine() == "n") endApp = true;
+                    Console.WriteLine("\n");
+                    calculator.Finish();
+                    return;
+                }
+
+                if (operation == "tan")
+                {
+                    Trigonometry(operation, number1, number2, result);
+                    if (Console.ReadLine() == "n") endApp = true;
+                    Console.WriteLine("\n");
+                    calculator.Finish();
+                    return;
+                }
+           
+                if (operation == "cot")
+                {
+                    Trigonometry(operation, number1, number2, result);
+                    if (Console.ReadLine() == "n") endApp = true;
+                    Console.WriteLine("\n");
+                    calculator.Finish();
+                    return;
+                }
+
+                // Просим пользователя ввести и первое, и второе число.
+                double clearingNumber1 = 0;
+                double clearingNumber2 = 0;
+                Console.Write("Type a first number, and then press Enter: ");
+                number1 = Console.ReadLine();
+                while (!double.TryParse(number1, out clearingNumber1))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    number1 = Console.ReadLine();
+                }
+                Console.Write("Type a second number, then press Enter: ");
+                number2 = Console.ReadLine();
+                while (!double.TryParse(number2, out clearingNumber2))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    number2 = Console.ReadLine();
+                }
                 try
                 {
-                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(clearingNumber1, clearingNumber2, operation);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -66,17 +141,13 @@ namespace CalculatorProgram
                 {
                     Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
                 }
-
-                Console.WriteLine("------------------------\n");
-
-                // Ждем, пока пользователь ответит, прежде чем закрыть.
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n") endApp = true;
 
-                Console.WriteLine("\n"); // Дружественный межстрочный интервал.
+                Console.WriteLine("\n");
+                calculator.Finish();
+                return;
             }
-            calculator.Finish();
-            return;
         }
     }
 }
