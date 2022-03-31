098e1101da6097231f0efa598a5554d6b2e1e2d5
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-03-31 21:15:28 +0300

    Add files via upload

---
+++ ScrumBoard/Columns/Columns.cs
@@ -0,0 +1,34 @@
+﻿namespace ScrumBoard.Body
+{
+    public class Column : ColumnInterface
+    {
+        public string Title { get; set; }
+
+        public Column(string title)
+        {
+            Title = title;
+            _tasks = new();
+        }
+        public TaskInterface? Find_task_by_title(string title)
+        {
+            return _tasks.Find(task => task.Title == title);
+        }
+
+        public void Delete_task_by_title(string title)
+        {
+            _tasks.RemoveAll(task => task.Title == title);
+        }
+
+        public void Add_task(TaskInterface task)
+        {
+            _tasks.Add(task);
+        }
+
+        public IReadOnlyCollection<TaskInterface> Find_tasks()
+        {
+            return _tasks;
+        }
+
+        private List<TaskInterface> _tasks;
+    }
+}
