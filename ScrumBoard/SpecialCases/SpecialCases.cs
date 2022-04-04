883a9171c22e2a0875d1a4e13f1a7cebed79dd01
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-04-04 23:25:21 +0300

    Update SpecialCases.cs

--- ScrumBoard/SpecialCases/SpecialCases.cs
+++ ScrumBoard/SpecialCases/SpecialCases.cs
@@ -1,8 +1,8 @@
-﻿namespace ScrumBoard.SpecialCases
+namespace ScrumBoard.SpecialCases
 {
-    public class MaximumNumbers : System.Exception
+    public class MaximumColumns : System.Exception
     {
-        public MaximumNumbers()
+        public MaximumColumns()
             : base("the number of columns on the board is exceeded, the maximum count of columns is 10")
         {
         }
