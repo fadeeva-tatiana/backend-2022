1ce1e40ab3279bc8b9e9893af361850917d56c0d
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-04-25 22:37:18 +0300

    Add files via upload

---
+++ WebScrumBoard/Repositories/ScrumBoardRepository.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Task = ScrumBoard.Creator;
+using WebScrumBoard.DTO;
+using TasksDTO;
+using ScrumBoard.Body;
+
+namespace WebScrumBoard.Models;
+
+public class ScrumBoardRepository : ScrumBoardRepositoryInterface
+{
+    private readonly Memory_cacheInterface _memoryCache;
+
+    public ScrumBoardRepository(Memory_cacheInterface memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public void Add_board(CreateBoardDTO param)
+    {
+        List<BoardInterface> boards;
+        {
+            boards = new List<BoardInterface>();
+        }
+
+        boards.Add(new Board(param.Name));
+
+        _memoryCache.Set("boards", boards);
+    }
+
+    public BoardDTO Get_board(string boardGUID)
+    {
+        return new BoardDTO(GetListBoards()[Get_index_board(boardGUID)]);
+    }
+
+    public IEnumerable<BoardDTO> GetAllBoard()
+    {
+        return GetListBoards().Select(board => new BoardDTO(board));
+    }
+
+    public void Remove_board(string boardGUID)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        for (int i = 0; i < boards.Count; i++)
+        {
+            if (boards[i].GUID == boardGUID)
+            {
+                boards.RemoveAt(i);
+                _memoryCache.Set("boards", boards);
+                return;
+            }
+        }
+    }
+
+    public void Add_column(string boardGUID, CreateColumnDTO param)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        boards[Get_index_board(boardGUID)].Add_column(new Column(param.Name));
+
+        _memoryCache.Set("boards", boards);
+    }
+
+    public void Edit_column(string boardGUID, EditColumnDTO param)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        boards[Get_index_board(boardGUID)].Rename_column(param.GUID, param.Name);
+
+        _memoryCache.Set("boards", boards);
+    }
+
+    public void Add_task(string boardGUID, CreateTaskDTO param)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        boards[Get_index_board(boardGUID)].Add_task_to_column(
+            new Task(param.Name, param.Description, GetTaskPriority(param.Priority))
+        );
+
+        _memoryCache.Set("boards", boards);
+    }
+
+    public void Remove_task(string boardGUID, string taskGUID)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        boards[Get_index_board(boardGUID)].Delete_task(taskGUID);
+
+        _memoryCache.Set("boards", boards);
+    }
+
+    public void Move_task(string boardGUID, string taskGUID, MoveTaskDTO param)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        boards[Get_index_board(boardGUID)].Move_task(param.columnGUID, taskGUID);
+
+        _memoryCache.Set("boards", boards);
+    }
+
+    private int Get_index_board(string boardGUID)
+    {
+        List<BoardInterface> boards = GetListBoards();
+
+        for (int i = 0; i < boards.Count; i++)
+        {
+            if (boards[i].GUID == boardGUID)
+            {
+                return i;
+            }
+        }
+    }
+
+    private List<BoardInterface> GetListBoards()
+    {
+        _memoryCache.TryGetValue("boards", out List<BoardInterface> boards);
+        return boards;
+    }
+
+    private static Task_priority GetTaskPriority(int priority)
+    {
+        return priority switch
+        {
+            0 => Task_priority.EASY,
+            1 => Task_priority.MEDIUM,
+            2 => Task_priority.HARD,
+        };
+    }
+}
---
+++ WebScrumBoard/Repositories/ScrumBoardRepositoryInterface.cs
@@ -0,0 +1,31 @@
+﻿using TasksDTO;
+using WebScrumBoard.DTO;
+
+namespace WebScrumBoard.Models;
+
+public interface ScrumBoardRepositoryInterface
+{
+    public void Add_board(CreateBoardDTO param);
+
+    public BoardDTO Get_board(string guid);
+
+    public IEnumerable<BoardDTO> GetAllBoard();
+
+    public void Remove_board(string boardGUID);
+
+
+    public void Add_column(string boardGUID, CreateColumnDTO param);
+
+    public void Edit_column(string boardGUID, EditColumnDTO param);
+
+    public void Remove_column(string boardGUID, string columnGUID);
+
+
+    public void Add_task(string boardGUID, CreateTaskDTO param);
+
+    public void Edit_task(string boardGUID, EditTaskDTO param);
+
+    public void Remove_task(string boardGUID, string taskGUID);
+
+    public void Move_task(string boardGUID, string taskGUID, MoveTaskDTO param);
+}
