1713fb486670e35b7b7a35eac0133677587d80d3
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-04-25 22:33:54 +0300

    Add files via upload

---
+++ WebScrumBoard/DTO/ColumnsDTO.cs
@@ -0,0 +1,31 @@
+﻿using ScrumBoard.Body;
+using TasksDTO;
+
+namespace WebScrumBoard.DTO;
+
+public class ColumnsDTO
+{
+    public ColumnsDTO(ColumnInterface column)
+    {
+        GUID = column.GUID;
+        Name = column.Title;
+        Tasks = column.Find_tasks().Select(task => new TaskDTO(task));
+    }
+
+    public string GUID { get; set; }
+
+    public string Name { get; set; }
+
+    public IEnumerable<TaskDTO> Tasks { get; set; }
+}
+
+public class EditColumnDTO
+{
+    public string? GUID { get; set; }
+    public string? Name { get; set; }
+}
+
+public class CreateColumnDTO
+{
+    public string? Name { get; set; }
+}
---
+++ WebScrumBoard/DTO/TasksDTO.cs
@@ -0,0 +1,39 @@
+﻿using ScrumBoard.Creator;
+using ScrumBoard.Body;
+
+namespace TasksDTO;
+
+public class CreateTaskDTO
+{
+    public string? Name { get; set; }
+    public int Priority { get; set; }
+    public string? Description { get; set; }
+}
+
+public class EditTaskDTO
+{
+    public string? Name { get; set; }
+    public int Priority { get; set; }
+    public string? Description { get; set; }
+    public string? GUID { get; set; }
+}
+
+public class MoveTaskDTO
+{
+    public string? columnGUID { get; set; }
+}
+
+public class TaskDTO
+{
+    public TaskDTO(TasksInterface task)
+    {
+        GUID = task.GUID;
+        Name = task.Name;
+        Description = task.Description;
+        Priority = task.Priority.ToString();
+    }
+    public string Name { get; set; }
+    public string Priority { get; set; }
+    public string Description { get; set; }
+    public string GUID { get; set; }
+}
