207cb5e81792bf6a003fda2d58653c804ef991c0
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-04-27 21:59:26 +0300

    Add files via upload

---
+++ WebScrumBoard/DTO/BoardDTO.cs
@@ -0,0 +1,22 @@
+﻿using ScrumBoard.Body;
+
+namespace WebScrumBoard.DTO;
+
+public class BoardDTO
+{
+    public BoardDTO(BoardInterface board)
+    {
+        ID = board.Identifier;
+        Title = board.Title;
+        Columns = board.Find_columns().Select(column => new ColumnsDTO(column));
+    }
+
+    public string ID { get; set; }
+    public string Title { get; set; }
+    public IEnumerable<ColumnsDTO> Columns { get; set; }
+}
+
+public class CreateBoardDTO
+{
+    public string? Title { get; set; }
+}
---
+++ WebScrumBoard/DTO/TasksDTO.cs
@@ -0,0 +1,38 @@
+﻿using ScrumBoard.Body;
+
+namespace TasksDTO;
+
+public class CreateTaskDTO
+{
+    public string? Title { get; set; }
+    public int Priority { get; set; }
+    public string? Description { get; set; }
+}
+
+public class EditTaskDTO
+{
+    public string? Title { get; set; }
+    public int Priority { get; set; }
+    public string? Description { get; set; }
+    public string? ID { get; set; }
+}
+
+public class MoveTaskDTO
+{
+    public string? columnID { get; set; }
+}
+
+public class TaskDTO
+{
+    public TaskDTO(TaskInterface task)
+    {
+        ID = task.Identifier;
+        Title = task.Title;
+        Description = task.Description;
+        Priority = task.Priority.ToString();
+    }
+    public string Title { get; set; }
+    public string Priority { get; set; }
+    public string Description { get; set; }
+    public string ID { get; set; }
+}
