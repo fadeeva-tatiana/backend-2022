ac30f2bba5bb29e7114358d3bb01e752af67d815
fadeeva-tatiana <79213086+fadeeva-tatiana@users.noreply.github.com>
2022-06-01 21:08:55 +0300

    Add files via upload

---
+++ ScrumBoard/ScrumBoard/ScrumBoardCreator/ScrumBoardCreator.cs
@@ -0,0 +1,22 @@
+﻿using ScrumBoard.Body;
+
+namespace ScrumBoard.Creator
+{
+    public class ScrumBoardCreator
+    {
+        public static IBoard CreateBoard(string title) // Создание доски
+        {
+            return new Board(title);
+        }
+
+        public static IColumn CreateColumn(string title) // Создание колонки
+        {
+            return new Column(title);
+        }
+
+        public static ITask CreateTask(string title, string description, Task_priority priority) // Создание задания
+        {
+            return new Body.Task(title, description, priority);
+        }
+    }
+}
